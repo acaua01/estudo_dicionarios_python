1305d7e8b9b736f40a8ca9855c65003219969456
Acauã Vieira Payão <136847436+acaua01@users.noreply.github.com>
2023-06-23 15:56:45 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,37 @@
+""" 
+Programa Dicionario
+Descrição: Este programa apresenta a estrutura de dados dicionário em python para fins de aprendizado.
+Autor: Acauã Vieira Payão
+Data: 23/06/2023
+Versão: 0.0.1
+"""
+
+
+# Atribuição de variáveis
+
+tabela = {"Arroz" : 0.5,
+          "Batata" : 0.2,
+          "Carne" : 0.1}
+
+
+
+# Entrada de dados
+
+
+
+
+# Processamento de dados
+
+
+
+
+
+# Saída de dados
+
+# print(tabela["Arroz"])
+
+# print(tabela["Batata"])
+
+print(tabela["Alface"])
+
+# print(tabela{"Alface"})
